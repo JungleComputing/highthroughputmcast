b21da5bcd766984b0a2de87136ec94f05e35b03b
Mathijs den Burger <mdenburger@gmail.com>
2008-10-01 16:31:48 +0000

    added the RankPool and abstracted common pool functionality

--- src/mcast/p2p/Collective.java
+++ src/mcast/p2p/Collective.java
@@ -4,10 +4,44 @@ import ibis.ipl.IbisIdentifier;
 
 import java.util.List;
 
-public interface Collective {
+public class Collective {
 
-    public String getName();
+    private String name;
+    private List<IbisIdentifier> members;
     
-    public List<IbisIdentifier> getMembers();
+    public Collective(String name, List<IbisIdentifier> members) {
+        this.name = name;
+        this.members = members;
+    }
+    
+    public List<IbisIdentifier> getMembers() {
+        return members;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (o instanceof Collective) {
+            Collective rhs = (Collective)o;
+            return name.equals(rhs.name);
+        } else {
+            return false;
+        }
+    }
+    
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
     
 }
---
+++ src/mcast/p2p/RankPool.java
@@ -0,0 +1,71 @@
+package mcast.p2p;
+
+import ibis.ipl.Ibis;
+import ibis.ipl.IbisIdentifier;
+import ibis.ipl.Registry;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Pool where the collectives are based on the names of the clusters.
+ */
+public class RankPool extends AbstractPool implements Pool {
+
+    private String[] collectiveNames;
+    
+    public RankPool(String name, Ibis ibis, String[] collectiveNames, int myRank) 
+    throws IOException {
+        super(name, collectiveNames.length);
+        
+        this.collectiveNames = new String[collectiveNames.length];
+        System.arraycopy(collectiveNames, 0, this.collectiveNames, 0, 
+                collectiveNames.length);
+        
+        Registry registry = ibis.registry();
+        
+        // elect each rank to determine the associated Ibis identifier
+        if (myRank >= 0 && myRank < collectiveNames.length) {
+            String myRankElection = Integer.toString(myRank);
+            registry.elect(myRankElection);
+        }
+
+        for (int i = 0; i < collectiveNames.length; i++) {
+            String rankElection = Integer.toString(i);
+            ibises[i] = registry.getElectionResult(rankElection);
+        }
+    }
+
+    @Override
+    protected String collectiveName(IbisIdentifier id) {
+        int rank = getRank(id);
+        return collectiveNames[rank];
+    }
+
+    private int getRank(IbisIdentifier id) {
+        for (int i = 0; i < ibises.length; i++) {
+            if (ibises[i].equals(id)) {
+                return i;
+            }
+        }
+        throw new RuntimeException("Unexpected error: could not find the " +
+        		"rank of " + id);
+        
+    }
+    
+    @Override
+    protected List<IbisIdentifier> collectiveMembers(IbisIdentifier ibis) {
+        List<IbisIdentifier> members = new ArrayList<IbisIdentifier>();
+        String collectiveName = collectiveName(ibis);
+        
+        for (int i = 0; i < ibises.length; i++) {
+            if (collectiveNames[i].equals(collectiveName)) {
+                members.add(ibises[i]);
+            }
+        }
+        
+        return members;
+    }
+
+}
